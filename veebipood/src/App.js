e49696189d39b208264fea14369a278cb003bb3c
KristelBurke <kristelvaher5@gmail.com>
2023-11-02 12:04:43 +0200

    reacti algus

---
+++ veebipood/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <img className="pilt" src="https://estonia.ee/wp-content/uploads/nobe_netist_4.jpg" alt=""/>
+      <button className="nupu-stiil">Vajuta mind</button>
+    </div>
+  );
+}
+
+export default App;
