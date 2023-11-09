18c7974fe1745723d391a4c968823aea3fea9757
KristelBurke <kristelvaher5@gmail.com>
2023-11-09 15:59:11 +0200

    firebase, useState, useRef

--- veebipood/src/pages/Avaleht.js
+++ veebipood/src/pages/Avaleht.js
@@ -1,20 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // div ja span on tühjused, aga div teeb uue rea, span ei tee
 
 function Avaleht() {
+    const [kogus, uuendaKogus] = useState (7);
+    const [laigitud, muudaLaigitud] = useState (false) ; // 0  - mittelaigitud, 1- laigitud
+    const [sonum, uuendaSonum] = useState ("uuenda kogust!");
+
+    const nulli = () => {
+        uuendaKogus(0);
+        uuendaSonum("Nullitud!");
+    }
+
+    const vahenda = () => {
+        uuendaKogus (kogus - 1);
+        uuendaSonum("Vähendatud!");
+    }
+
+    const suurenda = () => {
+        uuendaKogus (kogus + 1);
+        uuendaSonum("Suurendatud!");
+    }
+
+    
     return (
         <div>
-            <img src="/mittelaigitud.svg" alt=""/>
-            <img src="/laigitud.svg" alt=""/>
-            <img src="/kuldne.svg" alt=""/>
-            <button>Pane laigituks</button>
-            <button>Pane mittelaigituks</button>
+            <div>{sonum}</div>
+            {laigitud === false && <img src="/mittelaigitud.svg" alt=""/>}
+            {laigitud === true && <img src="/laigitud.svg" alt=""/>}
+            {kogus > 10 && <img src="/kuldne.svg" alt=""/>}
+            <button onClick={() => muudaLaigitud (true)}>Pane laigituks</button>
+            <button onClick={() => muudaLaigitud (false)}>Pane mittelaigituks</button>
             <br/> 
+
+
             <br/>
-            <button>-</button>
-            <span>7</span>
-            <button>+</button>
+            {kogus !== 0 && <button onClick={nulli}>Tagasi nulli</button>}
+            <br/>
+            {<button disabled={kogus === 0} onClick={vahenda}>-</button>}
+            <span>{kogus}</span>
+            <button onClick={suurenda}>+</button>
         </div>
     )
 }
--- veebipood/src/pages/LisaToode.js
+++ veebipood/src/pages/LisaToode.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 function LisaToode() {
+    const [sonum, maaraSonum] = useState("Lisa toode");
+    const inputiLuger = useRef();
+
+    function lisa() {
+        if(inputiLuger.current.value === "") {
+            maaraSonum("Tuhja nimetusega ei saa toodet lisada");
+
+        } else {
+            maaraSonum("Toode lisatud " + inputiLuger.current.value)
+        }    
+    }
+
     return (
         <div>
+            <div>{sonum}</div>
             <label>Uus toode</label> <br/>
-            <input type="text"/> <br/>
-            <button>Sisesta</button> <br/>
+            <input ref={inputiLuger} type="text"/> 
+            <br/>
+            <button onClick={() => lisa()}>Sisesta</button> <br/>
         </div>
     )
 }
