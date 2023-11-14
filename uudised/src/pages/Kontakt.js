10bd0dedf610cfa1fde7e1208dd0daf786534d25
KristelBurke <kristelvaher5@gmail.com>
2023-11-14 15:42:10 +0200

    uudised

--- uudised/src/pages/Kontakt.js
+++ uudised/src/pages/Kontakt.js
@@ -6,13 +6,21 @@ function Kontakt() {
             <div>See on avaleht, nahtav localhost:3000/kontakt aadressil</div>
             <div>Vota meiega uhendust</div>
             <br/>
+
             <div>Kristiine keskus</div>
             <div>+372123123</div>
             <div>Endla 45, Tallinn</div>
             <br/>
+
             <div>Ulemiste keskus</div>
             <div>+372123123</div>
             <div>Suur-Sojamae 4, Tallinn</div>
+            <br/>
+
+            <div>Tasku keskus</div>
+            <div>+372123123</div>
+            <div>Turu 2, Tartu</div>
+
         </div>
     )
 }
--- uudised/src/pages/Meist.js
+++ uudised/src/pages/Meist.js
@@ -6,14 +6,26 @@ function Meist() {
             <div>See on meist leht, nahtav localhost:3000/meist aadressil</div>
             <div>Meie tootajad</div>
             <br/>
+
             <div>Mari Maasikas</div>
             <div>+372123123</div>
             <div>Uudised</div>
             <br/>
+
             <br/>
             <div>Virve Vihmavari</div>
             <div>+372123123</div>
             <div>Kujundus</div>
+
+            <br/>
+            <div>Hunt Kriimsilm</div>
+            <div>+372123123</div>
+            <div>Reporter</div>
+
+            <br/>
+            <div>Kalle Kaalikas</div>
+            <div>+372123123</div>
+            <div>Valisuudised</div>
         </div>
     )
 }
