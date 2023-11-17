e3b8151c83493ddeb6055143d9fe2c46c9c372db
KristelBurke <kristelvaher5@gmail.com>
2023-11-17 20:11:50 +0200

    tegelased

---
+++ tegelased/src/pages/Avaleht.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+function Avaleht() {
+    return (
+        <div>
+<br/>
+            <div>
+                <div>Mickey</div>
+                <div>Mouse</div>
+                <div>Disneyland</div>
+            </div>
+<br/>
+            <div>
+                <div>Minnie</div>
+                <div>Mouse</div>
+                <div>Disneyland</div>
+            </div>
+<br/>
+            <div>
+                <div>Winnie</div>
+                <div>Pooh</div>
+                <div>Hundered Acre Wood</div>
+            </div>
+<br/>
+            <div>
+                <div>Roo</div>
+                <div>Kangaroo</div>
+                <div>Hundered Acre Wood</div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Avaleht
