0d167c13485b51cfedc6c8eceb3c1df2fa1b5e4a
KristelBurke <kristelvaher5@gmail.com>
2023-12-04 15:59:29 +0200

    arrays, uuenda ostukorv

--- veebipood/src/pages/Ostukorv.js
+++ veebipood/src/pages/Ostukorv.js
@@ -1,14 +1,46 @@
-import { Link, } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function Ostukorv() {
+    const [ostukorv, uuendaOstukorv] = useState(["Coca", "Fanta", "Sprite"]);
+
+    const kustuta = () => {
+        ostukorv.splice(0,1);
+        uuendaOstukorv(ostukorv.slice());
+    }
+
     return (
         <div>
-            <div>
-                Ostukorv on tühi
-            </div>
-            <Link to="/avaleht">
-                <button>Avalehele</button>
-            </Link>
+            <br/>
+            <button onClick={()=> uuendaOstukorv(["Coca", "Fanta", "Sprite"])}>Tagasi originaali </button>
+            <button onClick={()=> uuendaOstukorv(["Coca", "Fanta"])}>Jata alles Coca ja Fanta</button>
+            <button onClick={()=> uuendaOstukorv(["Sprite", "Fanta"])}>Jata alles Sprite ja Fanta </button>
+            <button onClick={()=> uuendaOstukorv(["Coca", "Sprite"])}>Jata alles Coca ja Sprite </button>
+            <button onClick={()=> uuendaOstukorv(["Coca"])}>Jata alles Coca </button>
+            <button onClick={()=> uuendaOstukorv(["Fanta"])}>Jata alles Fanta</button>
+            <button onClick={()=> uuendaOstukorv(["Sprite"])}>Jata alles Sprite </button>
+            <button onClick={kustuta}>Kustuta esimene</button>
+            
+            <button onClick={()=> uuendaOstukorv([])}>Tuhjenda</button>
+
+            {ostukorv.length > 0 && <div>Ostukorvis on {ostukorv.length} ese(t)</div>}
+
+            {ostukorv.map( toode => 
+                <div>
+                    <span>{toode}</span>
+                    <button>X</button>
+                </div> 
+                )}
+
+            {ostukorv.length === 0 && 
+                <div>
+                    <div>Ostukorv on tuhi</div>
+                    <Link to="/avaleht">
+                        <button>Avalehele</button>
+                    </Link>
+                </div>
+            }
+
         </div>
     )
 }
--- veebipood/src/pages/Tootajad.js
+++ veebipood/src/pages/Tootajad.js
@@ -3,6 +3,19 @@ import React from 'react'
 function Tootajad() {
     const tootajad = ["Pille", "Kalle", "Malle", "Sille"];
 
+  // Array (Massiiv, List) väljakuvamiseks on vaja see muutuja, mille sees on Array
+  // (tema väärtus on kandilisest sulust kandilise suluni)
+  // .map() abil, mille panen muutuja lõppu, loogeliste sulgude sisse
+  // {MUUTUJA.map()}
+  // .map() on tsükkel ehk käiakse ükshaaval läbi kõik väärtused mis asuvad Array sees
+  // .map(muutuja => )
+  // muutuja sisse tekib ükshaaval Array ühe elemendi väärtus
+  // {tootajad.map(element => <button>{element}</button>)}
+  // ["Pille", "Kalle", "Malle", "Sille"].map("Pille" => <button>Pille</button> )
+  // ["Pille", "Kalle", "Malle", "Sille"].map("Kalle" => <button>Kalle</button> )
+  // ["Pille", "Kalle", "Malle", "Sille"].map("Malle" => <button>Malle</button> )
+  // ["Pille", "Kalle", "Malle", "Sille"].map("Sille" => <button>Sille</button> )
+
     return (
         <div>
             {tootajad.map(element => <button>{element}</button>)}
