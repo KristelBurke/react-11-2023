2b3ffdb60a1b7126ea4c73cc854d4510ee91eb60
KristelBurke <kristelvaher5@gmail.com>
2023-12-13 20:00:54 +0200

    Hinnad.js array functions

--- veebipood/src/pages/Hinnad.js
+++ veebipood/src/pages/Hinnad.js
@@ -8,11 +8,39 @@ function Hinnad() {
         uuendaHinnad(hinnad.slice());
     }
 
+    const hindKahanevalt =() => {
+        hinnad.sort((a,b) => b - a );
+        uuendaHinnad(hinnad.slice());
+    }
+    const hindNaguSonaAZ =() => {
+        hinnad.sort((a,b) => a.toString().localeCompare(b.toString()));
+        uuendaHinnad(hinnad.slice());
+    }
+
+    const filtreeriVah50 = () => {
+        const vastus = hinnad.filter(hind => hind >= 50);
+        uuendaHinnad(vastus);
+    }
+    const filtreeriSisaldab5 = () => {
+        const vastus = hinnad.filter(hind => hind.toString().includes("5"));
+        uuendaHinnad(vastus);
+    }
+
+    const filtreeriAlgab1 = () => {
+        const vastus = hinnad.filter(hind => hind.toString().startsWith("1"));
+        uuendaHinnad(vastus);
+    }
+
 
     return (
         <div>
-          {hinnad.map(hind => <div>{hind}</div>)}
+          {hinnad.map(hind =><div>{hind}</div>)}
           <button onClick={hindKasvavalt}>Hind kasvavalt</button>
+          <button onClick={hindKahanevalt}>Hind kahanevalt</button>
+          <button onClick={hindNaguSonaAZ}>Hind a-z</button>
+          <button onClick={filtreeriVah50}>Vahemalt 50</button>
+          <button onClick={filtreeriSisaldab5}>Sisaldab 5</button>
+          <button onClick={filtreeriAlgab1}>Algavad 1-ga</button>
         </div>
     )
 }
