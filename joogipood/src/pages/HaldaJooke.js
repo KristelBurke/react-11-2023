e9a5a008b8fb21f9cff4887c5534e5f08292d8d9
KristelBurke <kristelvaher5@gmail.com>
2023-12-15 19:15:43 +0200

    22. Joogipood. Faili kustutamine andmete jagamiseks.

---
+++ joogipood/src/pages/HaldaJooke.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import joogidFailist from "../joogid.json";
+
+function HaldaJooke() {
+  const [joogid, uuendaJoogid] = useState(joogidFailist);
+
+
+  const kustuta = (index) => {
+    joogidFailist.splice(index, 1);
+    uuendaJoogid(joogidFailist.slice());
+}
+
+  return (
+    <div>
+      <br />
+      {joogid.map((jook, index) => (
+        <div key={index}>
+          {jook}
+          <button onClick={() => kustuta(index)}>X</button>
+        </div>
+      ))}
+      
+    </div>
+  );
+}
+
+export default HaldaJooke;
