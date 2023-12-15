66a05071b8170647f22d98d63c31b8a84871a85a
KristelBurke <kristelvaher5@gmail.com>
2023-12-15 19:26:11 +0200

    LisaJook sisu Avalehele

--- joogipood/src/pages/Avaleht.js
+++ joogipood/src/pages/Avaleht.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState, useRef }  from 'react';
+import joogidFailist from "../joogid.json";
 
 function Avaleht() {
+    const [joogid, uuendaJoogid] = useState(joogidFailist);
+    const jookRef = useRef();
+
+    const lisaUusJook = () =>{
+        joogidFailist.push(jookRef.current.value);
+        uuendaJoogid(joogidFailist.slice())
+    }
     return (
-        <div>
-            Avaleht
-        </div>
+    <div>
+        <br/>
+        {joogid.map((jook, index) => (
+        <div key={index}>{jook}</div>
+      ))}
+      <br/>
+        <label>Lisa uus jook</label> <br/>
+        <input ref={jookRef} type="text"/>
+        <button onClick={lisaUusJook}>Lisa</button>
+    </div>
     )
 }
 
--- joogipood/src/pages/LisaJook.js
+++ joogipood/src/pages/LisaJook.js
@@ -1,25 +1,11 @@
-import React, { useState, useRef } from 'react';
-import joogidFailist from "../joogid.json";
+import React from 'react';
 
 function Lisa() {
-    const [joogid, uuendaJoogid] = useState(joogidFailist);
-    const jookRef = useRef();
-
-    const lisaUusJook = () =>{
-        joogidFailist.push(jookRef.current.value);
-        uuendaJoogid(joogidFailist.slice())
-    }
+    
 
     return (
         <div>
-        <br/>
-        {joogid.map((jook, index) => (
-        <div key={index}>{jook}</div>
-      ))}
-      <br/>
-        <label>Lisa uus jook</label> <br/>
-        <input ref={jookRef} type="text"/>
-        <button onClick={lisaUusJook}>Lisa</button>
+         Lisa jook
         </div>
     )
 }
