b290a4cc55381ce3c04cadbc7b4d940674850e30
KristelBurke <kristelvaher5@gmail.com>
2023-12-28 19:46:35 +0200

    Tegelased nr 29

--- tegelased/src/pages/Avaleht.js
+++ tegelased/src/pages/Avaleht.js
@@ -1,33 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Avaleht() {
+    const tegelased = [
+        {eesnimi: "Mickey", perenimi:"Mouse", elukoht:"Disneyland"},
+        {eesnimi: "Minnie", perenimi:"Mouse", elukoht:"Disneyland"},
+        {eesnimi: "Winnie", perenimi:"Pooh", elukoht:"Hundered Acre Wood"},
+        {eesnimi: "Roo", perenimi:"Kangaroo", elukoht:"Hundered Acre Wood"},
+        {eesnimi: "Scooby", perenimi:"Doo", elukoht:"Crystal Cove"},
+
+    ];
+    const [klikitudNimi, uuendaKlikitudNimi] = useState("");
+
+    const kuvaNimi = (tegelane) => {
+        // console.log(tegelane.eesnimi);
+        uuendaKlikitudNimi(tegelane.eesnimi);
+    }
+       
     return (
         <div>
-<br/>
-            <div>
-                <div>Mickey</div>
-                <div>Mouse</div>
-                <div>Disneyland</div>
-            </div>
-<br/>
-            <div>
-                <div>Minnie</div>
-                <div>Mouse</div>
-                <div>Disneyland</div>
-            </div>
-<br/>
-            <div>
-                <div>Winnie</div>
-                <div>Pooh</div>
-                <div>Hundered Acre Wood</div>
-            </div>
-<br/>
-            <div>
-                <div>Roo</div>
-                <div>Kangaroo</div>
-                <div>Hundered Acre Wood</div>
-            </div>
-
+            {klikitudNimi !== "" && <div>Klikkisid tegelase {klikitudNimi} peal</div>}
+            {tegelased.map(tegelane =>
+                <div>
+                    <br />
+                    <div>{tegelane.eesnimi}</div>
+                    <div>{tegelane.perenimi}</div>
+                    <div>{tegelane.elukoht}</div>
+                    <button onClick={() => kuvaNimi(tegelane)}>Kuva nimi</button>
+                </div>
+                
+                )}
         </div>
     )
 }
