988481e284da0e4829a8b1c25d739e9a521229f1
KristelBurke <kristelvaher5@gmail.com>
2023-12-31 16:20:48 +0200

    not found page designed, maintain page list of products

--- webshop/src/pages/admin/MaintainProducts.jsx
+++ webshop/src/pages/admin/MaintainProducts.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react';
+import productsFromFile from "../../data/products.json"
 
 function MaintainProduct() {
+  const [products, setProducts] = useState(productsFromFile);
+
+  const emptyProducts = () => {
+    setProducts([]); 
+  };
+
   return (
-    <div>MaintainProduct</div>
+    <div>
+      <button onClick={emptyProducts}>Delete</button>
+      {products.map(product => 
+          <div key={product.id}>
+            <img src={product.image} alt="" />
+            <div> {product.name} </div>
+            <div> {product.price} </div>
+            <div> {product.description} </div>
+            <div> {product.category} </div>
+            <div> {product.active} </div>
+            <br />
+            <br />
+          </div>
+          )}
+    </div>
   )
 }
 
--- webshop/src/pages/global/NotFound.jsx
+++ webshop/src/pages/global/NotFound.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom'; 
 
 function NotFound() {
   return (
-    <div>NotFound</div>
-  )
+    <div>
+      <h2>Lost your way?</h2>
+      <img className="notfound_img" src="/notFound.jpg" alt="" />
+      <br />
+      <br />
+      <Link to="/">
+        <button>Go back to Home</button>
+      </Link>
+    </div>
+  );
 }
 
-export default NotFound
+export default NotFound;
