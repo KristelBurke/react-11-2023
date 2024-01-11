b9f405ec4f93e68f51f7da91a59c6533e916166a
dutchessk <148763839+KristelBurke@users.noreply.github.com>
2024-01-11 16:08:46 +0200

    28.Uudiste lisamine labi LocalStorage'i

--- uudised/src/pages/LisaUudis.js
+++ uudised/src/pages/LisaUudis.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useRef } from 'react';
 
 function LisaUudis() {
-    return (
-        <div>
-            Lisa uudis
-        </div>
-    )
+    const uudiseRef = useRef();
+
+    const lisaUusUudis = () => {
+        const uudised = JSON.parse(localStorage.getItem("uudised")) || [];
+        uudised.push(uudiseRef.current.value);
+        localStorage.setItem("uudised", JSON.stringify(uudised));
+    }
+
+  return (
+    <div>
+        <br />
+        <label>Uudise nimi</label><br />
+        <input type="text" ref={uudiseRef} /><br />
+        <button onClick={() => lisaUusUudis()}>Lisa uudis</button>
+    </div>
+  )
 }
 
 export default LisaUudis
